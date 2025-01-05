b404cfcd5e34df90847e9d5721125eae9206c6f8
Ivan <ivanjovovic98@gmail.com>
2025-01-05 18:10:01 +0100

    Sakriven API

--- app.py
+++ app.py
@@ -48,5 +48,6 @@ def home():
 
 # Pokretanje aplikacije
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
 
